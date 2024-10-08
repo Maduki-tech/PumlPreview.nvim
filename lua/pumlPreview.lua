599992d05515f8448162f3e23be4530aac0ddc93
Maduki-tech <d.schlueter1011@gmail.com>
2024-10-08 20:33:17 +0200

    Textrendering for plantuml

--- lua/pumlPreview.lua
+++ lua/pumlPreview.lua
@@ -2,11 +2,14 @@ local logger = require("util.logger")
 local text = require("renderer.text")
 
 local M = {}
+local filename = "pumlPreview.lua"
 
 ---@class preview.Renderer
 ---@field command string
 ---@field stdin boolean
 ---@field stdout boolean
+
+-- TODO: Check for Types so it will be expandable
 
 ---@return TextRenderer
 local function create_renderer(type, opts)
@@ -15,18 +18,23 @@ local function create_renderer(type, opts)
 end
 
 function M.setup()
+	--- Deafault options
 	local default_opts = {
 		previewer = {},
 		render_on_write = false,
 	}
+	-- TODO: make the user able to add there own config
+
 	vim.api.nvim_create_user_command("PumlPreview", function()
 		M.PumlPreview(default_opts)
 	end, {})
 end
 
+--- Start the PumlPreview
 function M.PumlPreview(opts)
-	logger:log("PumlPreview")
+	logger:log(filename, "PumlPreview", "Starting PumlPreview")
 	local renderer = create_renderer("text", opts)
+
 	renderer:show()
 end
 
--- lua/renderer/text.lua
+++ lua/renderer/text.lua
@@ -1,6 +1,8 @@
 local logger = require("util.logger")
+local utils = require("util.utils")
 
 local M = {}
+local filename = "renderer/text.lua"
 
 ---@class TextRenderer
 ---@field buf number
@@ -25,23 +27,67 @@ end
 
 ---@param content string[]
 function M.TextRenderer:write(content)
-	logger:log("TextRenderer:Write")
+	assert(type(content) == "table", "Content must be a table")
 	vim.api.nvim_buf_set_lines(self.buf, 0, -1, false, content)
-end
-
-function M.TextRenderer:show()
-	logger:log("TextRenderer:Show")
-	self:create_window()
 end
 
 ---@private
 function M.TextRenderer:create_window()
 	if not (self.win and vim.api.nvim_win_is_valid(self.win)) then
-		vim.api.nvim_command("vsplit")
-		self:write({ "Hello, World!" })
+		vim.api.nvim_command(self.split_cmd)
 		self.win = vim.api.nvim_get_current_win()
 		vim.api.nvim_win_set_buf(self.win, self.buf)
 	end
+end
+
+---@private
+function M.TextRenderer:convertPumlToText()
+	local currentOpenFilePath = vim.api.nvim_buf_get_name(0)
+	logger:log(filename, "TextRenderer:convertPumlToText", currentOpenFilePath)
+
+	local handle = io.popen("cat " .. currentOpenFilePath .. " | plantuml -tutxt -pipe")
+	assert(handle, "Failed to open handle")
+	local output = handle:read("*a")
+	handle:close()
+
+	logger:log(filename, "TextRenderer:convertPumlToText", output)
+	local lines = self:convertOutputToTable(output)
+	self:write(lines)
+end
+
+---@private
+---@param ... string
+---@return string[]
+function M.TextRenderer:convertOutputToTable(...)
+	local processed = {}
+
+	for i = 1, select("#", ...) do
+		local item = select(i, ...)
+		if type(item) == "table" then
+			item = vim.inspect(item)
+		end
+		table.insert(processed, item)
+	end
+
+	local lines = {}
+	for _, line in ipairs(processed) do
+		local split = utils.split(line, "\n")
+		for _, l in ipairs(split) do
+			if not utils.is_white_space(l) then
+				local ll = utils.trim(utils.remove_duplicate_whitespace(l))
+				table.insert(lines, ll)
+			end
+		end
+	end
+
+	return lines
+end
+
+function M.TextRenderer:show()
+	logger:log(filename, "TextRenderer:Show", "Showing TextRenderer")
+	-- TODO: Generate the text file with pumlcli tool
+	self:convertPumlToText()
+	self:create_window()
 end
 
 return M
